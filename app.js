75218e59bb0022af944f327e3457572064d48d02
Gary <garyarneys@gmail.com>
2023-02-26 13:15:45 -0600

    Added ternary to catch the posts errors, no idea why app.use wouldn't catch any errors otherwise.

--- app.js
+++ app.js
@@ -7,21 +7,14 @@ app.use(morgan('dev'));
 
 app.use(express.static('public'));
 
+
+
 app.get("/", (req, res) => {
   
 const posts = postBank.list();
-console.log(postBank.list,'consolelog');
-// const html = `<!DOCTYPE html>
-// <html>
-// <head>
-//   <title>Wizard News</title>
-//   </head>
-//   <body>
-//     <ul>
-//     ${posts.map(post => `<li> ${post.title}</li>`)}
-//     </ul>
-//     </body>
-//     </html>`
+
+
+// console.log(postBank.list.post,'consolelog');
 
 const html = `<!DOCTYPE html>
 <html>
@@ -52,27 +45,15 @@ res.send(html);
 
 });
 
+
+
+
+
 app.get('/posts/:id', (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
-//   res.send(`<!DOCTYPE html>
-//   <html>
-//   <head>
-//    <title>Post #${post.id}, ${post.title}</title>
-//    <link rel="stylesheet" href="/style.css" />
-//    </head>
-//    <body>
-//    <
-//    <header><img src="/logo.png"/>Wizard News</header>
-//   <p>${post.title}</p>
-//   <p>${post.name}</p>
-//   <p>${post.content}</p>
-//   <p>${post.date}</p>
-//   <p>${post.upvotes}</p>`);
 
-
-
-// });
+  post.id ?
 res.send(`<!DOCTYPE html>
 <html>
 <head>
@@ -96,11 +77,21 @@ res.send(`<!DOCTYPE html>
    
   </div>
 </body>
-</html>`)});
+</html>`)
+:
+next()
+});
+
+
+app.use((err, req, res, next) => {  //couldn't get this to catch an error, added ternary to the post area of app.get for post/:id.
+  console.log('ERROR HANDLER STARTED');
+  console.error(err.stack)
+  res.send(`<p>Something broke!<p>`)
+})
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-//Don't forget to use join to get rid of the commas. Moving on for now.
